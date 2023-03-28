e924018521344a027aa4a431f2f83c6754109e72
Zoey Baker <zoeybaker625@gmail.com>
2023-03-28 14:08:29 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ var request = new HttpRequestMessage
     RequestUri = new Uri("https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5"),
     Headers =
     {
-        { "X-RapidAPI-Key", "d37724f31amsh5b7b473995b9699p1dd08djsn1424a0334a39" },
+        { "X-RapidAPI-Key", "insert_api_key" },
         { "X-RapidAPI-Host", "spotify23.p.rapidapi.com" },
     },
 };
